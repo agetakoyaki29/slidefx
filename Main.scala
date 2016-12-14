5955cfcd188f9b6c0679905c21b866bc571d7d19
Your Name <you@example.com>
2016-12-14 23:52:50 +0900

    init commit

---
+++ Main.scala
@@ -0,0 +1,135 @@
+// package foo.bar
+
+import java.io.File
+
+import javafx.fxml.FXML
+import javafx.application.{Application, Platform}
+import javafx.stage.{Window, Stage, StageStyle, FileChooser}
+import FileChooser.ExtensionFilter
+import javafx.scene.layout.{Region, BorderPane, StackPane}
+import javafx.scene.control.{TabPane, Tab, MenuBar}
+import javafx.event.ActionEvent
+
+
+object Main {
+	def main(args: Array[String]) {
+		println("helo")
+		Application.launch(classOf[MyApplication], args:_*)
+	}
+}
+
+class MyApplication extends Application {
+	override def start(stage: Stage) = new StageContaner(stage).show(new TopController)
+}
+
+class TopController extends StackPane with SceneController {
+	@FXML def onAction(event: ActionEvent) =  getStageContaner.moveScene(new EditerController)
+}
+
+class EditerController extends BorderPane with SceneController {
+	override def createMainMenu = Some(new MenuBarController(this))
+
+	@FXML var mainTabPane: TabPane = _
+	println(mainTabPane)
+	println(mainTabPane.isFocusTraversable)
+	mainTabPane.setFocusTraversable(false)
+	println(mainTabPane.isFocusTraversable)
+
+	def addAndSelectTab(tab: TextTabController) = {
+		mainTabPane.getTabs.add(tab)
+		val sm = mainTabPane.getSelectionModel
+		sm.select(tab)
+	}
+
+	def toClosetab = {
+		val index = mainTabPane.getSelectionModel.getSelectedIndex
+		if(index >= 0) mainTabPane.getTabs.remove(index)
+		else getSceneNonNull.getWindow.hide
+	}
+
+	def getSelectedItem = mainTabPane.getSelectionModel.getSelectedItem.asInstanceOf[TextTabController]
+}
+
+object TextTabController
+class TextTabController(val fileOp: Option[File]) extends Tab with RootedController {
+	this.getProperties.put(TextTabController, this)
+
+	fileOp match {
+		case Some(file) => setText(file.getName)
+		case None =>
+	}
+
+	def save = println("saved")
+}
+
+class MenuBarController(val editerController: EditerController) extends MenuBar with RootedController with StagedNode {
+	@FXML def onFileNew(event: ActionEvent) = editerController.addAndSelectTab(new TextTabController(None))
+	@FXML def onFileOpen(event: ActionEvent) = editerController.addAndSelectTab(new TextTabController( FCM.showOpenDialog(getSceneNonNull.getWindow) ))
+	@FXML def onFileReopen(event: ActionEvent) = println(getSceneNonNull.getFocusOwner)
+	@FXML def onFileSave(event: ActionEvent) = Option(editerController.getSelectedItem) match {
+		case None =>
+		case Some(tab) =>
+			tab.fileOp match {
+				case None => editerController.addAndSelectTab(new TextTabController( FCM.showSaveDialog(getSceneNonNull.getWindow) ))
+				case Some(file) => tab.save
+			}
+	}
+	@FXML def onFileSaveAs(event: ActionEvent) = println( FCM.showSaveDialog(getSceneNonNull.getWindow) )
+	@FXML def onFileCloseTab(event: ActionEvent) = editerController.toClosetab
+
+	@FXML def onFileExit(event: ActionEvent) = getSceneNonNull.getWindow.hide
+
+	@FXML def onSceneTop(event: ActionEvent) = getStageContaner.moveScene(new TopController)
+	@FXML def onSceneEditer(event: ActionEvent) = getStageContaner.moveScene(new EditerController)
+
+	@FXML def onHelpHelp(event: ActionEvent) = ???
+	@FXML def onHelpAbout(event: ActionEvent) = ???
+}
+
+// ----
+
+object FCM {
+	val that = new FCM(null)
+  def showOpenDialog(owner: Window) = that.showOpenDialog(owner)
+  def showSaveDialog(owner: Window) = that.showSaveDialog(owner)
+	def showSaveDialog(owner: Window, fileName: String) = that.showSaveDialog(owner, fileName)
+}
+
+class FCM private (private var initialDirectory: File) {
+	private lazy val opener = {
+		val fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().addAll(
+				new ExtensionFilter("Text Files", "*.txt"),
+				new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+				new ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+				new ExtensionFilter("All Files", "*.*")
+		)
+		fileChooser
+	}
+
+	def showOpenDialog(owner: Window) = {
+		val fileChooser = opener
+		if(initialDirectory != null) fileChooser.setInitialDirectory(initialDirectory)
+		val fileOp = Option( fileChooser.showOpenDialog(owner) )
+		if(fileOp.isDefined) initialDirectory = fileOp.get.getParentFile
+		fileOp
+	}
+
+	private lazy val saver = {
+		val fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().addAll(
+				new ExtensionFilter("Text Files", "*.txt")
+		)
+		fileChooser
+	}
+
+	def showSaveDialog(owner: Window): Option[File] = showSaveDialog(owner, "")
+	def showSaveDialog(owner: Window, fileName: String) = {
+		val fileChooser = saver
+		fileChooser.setInitialFileName(fileName)
+		if(initialDirectory != null) fileChooser.setInitialDirectory(initialDirectory)
+		val fileOp = Option( fileChooser.showSaveDialog(owner) )
+		if(fileOp.isDefined) initialDirectory = fileOp.get.getParentFile
+		fileOp
+	}
+}
---
+++ System.scala
@@ -0,0 +1,143 @@
+// package foo.bar
+
+import java.util.logging.Logger
+import java.io.IOException
+
+import javafx.fxml.FXMLLoader
+import javafx.stage.Stage
+import javafx.scene.{Scene, Node, Parent}
+import javafx.scene.layout.{BorderPane, AnchorPane}
+import javafx.scene.control.MenuBar
+
+import javafx.event.{ActionEvent, EventHandler}
+import javafx.animation.TranslateTransition
+import javafx.util.Duration
+
+
+object StageContaner
+class StageContaner(val stage: Stage) {
+	val rootPane = new BorderPane
+	val mainPane = new AnchorPane
+
+		// stage.initStyle(StageStyle.TRANSPARENT)
+	stage.getProperties.put(StageContaner, this)
+	// stage.setMaximized(true)
+
+	stage.setScene(new Scene(rootPane))
+	rootPane.setCenter(mainPane)
+
+	def show(firstScene: SceneController) = {
+		moveScene(firstScene)
+		stage.show
+	}
+
+	def moveScene(next: SceneController) = {
+		addMainPane(next)
+		setMainMenuBar(next.createMainMenu)
+		next.init
+
+		val prevOp = if(mainPane.getChildren.size <= 1) None
+								 else Some(mainPane.getChildren.get(mainPane.getChildren.size-2))
+
+		animateMove(next, prevOp)
+	}
+
+	def animateMove(next: Node, prevOp: Option[Node]) = {
+		val duration = Duration.seconds(1)
+		val width = mainPane.getLayoutBounds.getWidth
+
+		val slidIn = new TranslateTransition(duration, next)
+		slidIn.setFromX(width)
+		slidIn.setToX(0)
+		slidIn.play
+
+		prevOp match {
+			case Some(prev) =>
+				val slidOut = new TranslateTransition(duration, prev)
+				slidOut.setToX(-width)
+				slidOut.setOnFinished(_ => mainPane.getChildren.remove(prev))
+				slidOut.play
+			case None =>
+		}
+	}
+
+	def addMainPane(node: Node) = {
+		mainPane.getChildren.add(node)
+		AnchorPane.setBottomAnchor(node, 0)
+		AnchorPane.setLeftAnchor(node, 0)
+		AnchorPane.setRightAnchor(node, 0)
+		AnchorPane.setTopAnchor(node, 0)
+	}
+
+	def setMainMenuBar(option: Option[MenuBar]) = {
+		val before = Option(rootPane.getTop).map(_.asInstanceOf[MenuBar])
+		before match {
+			case Some(menuBar) => menuBar.setUseSystemMenuBar(false)
+			case None =>
+		}
+		option match {
+			case Some(menuBar) =>
+				menuBar.setUseSystemMenuBar(true)
+				rootPane.setTop(menuBar)
+			case None =>
+				rootPane.setTop(null)
+		}
+	}
+}
+
+trait RootedController {
+	val loader = new FXMLLoader(location)
+	loader.setRoot(this)
+	loader.setController(this)
+	try loader.load
+	catch { case e: IOException =>
+		Logger.getAnonymousLogger.warning("check fx:root, fx:controller attribute")
+		throw new RuntimeException("fxml loader can't load with location("+location+").", e)
+	}
+
+	def prefix = "C"
+
+	def fileName = {
+		val className = getClass.getSimpleName
+		val lastIndex = className.lastIndexOf(prefix)
+		if(lastIndex < 0) throw new RuntimeException("class name("+className+") doesn't include prefix("+prefix+").")
+		val fileName = className.substring(0, lastIndex) + ".fxml"
+		fileName
+	}
+
+	def location = {
+		val location = getClass.getResource(fileName)
+		if(location == null) throw new RuntimeException("class loader("+getClass+") can't find file with name ("+fileName+").")
+		location
+	}
+}
+
+trait StagedNode extends Node {
+	def getSceneNonNull = Mydef.nonNull(super.getScene, "this node("+this+") isn't part of a scene")
+
+	def getStage = getSceneNonNull.getWindow.asInstanceOf[Stage]
+
+	def getStageContaner = getSceneNonNull.getWindow.getProperties.get(StageContaner).asInstanceOf[StageContaner]
+}
+
+object StagedNode {
+	def getStageTest = {
+		val node = new BorderPane with StagedNode
+		val scene = new Scene(node)
+		node.getStage
+		val stage = new Stage()
+		stage.setScene(scene)
+		node.getStage
+	}
+}
+object Mydef {
+	def nonNull[T](any: T): T = nonNull(any, "("+any+") must not be null")
+	def nonNull[T](any: T, message: String): T = Option(any).getOrElse(throw new NullPointerException(message))
+
+	def doAny[T](any: T, f: T => Unit) = ??? // if non null then do
+}
+
+trait SceneController extends Parent with RootedController with StagedNode {
+	def createMainMenu: Option[MenuBar] = None
+	def init = {}
+}
